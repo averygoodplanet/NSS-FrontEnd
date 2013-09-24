8c82e65a57c5018fe9f5bb965c20931f0dc754c6
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-24 10:20:57 -0500

    Javascript Basics from 9.24 update.

--- 2013-09-23-Javascript-Basics/js/main.js
+++ 2013-09-23-Javascript-Basics/js/main.js
@@ -5,6 +5,10 @@
 
 var first_name = prompt('What is your first name?');
 var last_name = prompt('What is your last name?');
+var gender = prompt('Male or Female?');
+var age = prompt('Age?');
+var bday_month = prompt('What month were you born in?');
+var current_month = prompt("What month is it right now?")
 var full_name = first_name + " " + last_name;
 
 var test1 = parseFloat(prompt("Score for test1?"));
@@ -14,11 +18,56 @@ var test1 = parseFloat(prompt("Score for test1?"));
 var test2 = parseFloat(prompt("Score for test2?"));
 var test3 = parseFloat(prompt("Score for test3?"));
 
-var average = (test1 + test2 + test3) / 3;
+var sum = 0;
+sum += test1;
+sum += test2;
+sum += test3;
 
+var average = sum / 3;
 console.log("Your average was: "+average);
 
+
+if(average < 70)
+  console.log('failed')
+else if((average >= 70) && (average < 80))
+  console.log('you made a D');
+else if (average >= 80 && average < 90)
+  console.log('you made a B');
+else
+  console.log('you made an A');
 
 
 console.log("Your full name is: "+full_name);
 
+var null_variable = null, undefined_variable;
+/* created null_variable and assigned null to it.
+created undefined_variable but did not assigned anything to it. */
+
+if((first_name == 'Peter') && (last_name == "Himmelreich"))
+  console.log('hey, i recognize you!');
+
+if((gender == 'female') && (age >= 21))
+  console.log('free drinks, ladies night! woot!')
+else if((gender == 'male') && (age >= 21))
+  console.log('looks like you are buying!')
+else
+  console.log('not old enough to drink or indeterminate gender');
+
+var can_have_cake = (current_month == bday_month);
+var cake = (current_month == bday_month) ? "chocolate" : "vanilla";
+/* If this month matches the current month then you eat chocolate,
+otherwise you eat vanilla. */
+console.log("Marie Antoinette says that you are eating " + cake);
+
+
+switch(bday_month)
+{
+  case 'january':
+    console.log('you are a capricorn');
+    break;
+  case 'february':
+    console.log('you are a pisces');
+    break;
+  default:
+    console.log('you are not of this world, god speed!');
+}
