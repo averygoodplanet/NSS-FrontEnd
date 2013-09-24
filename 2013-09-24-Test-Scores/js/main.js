d6880148773885863d8a05e7df1c0cbe69547f2c
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-24 15:51:23 -0500

    Test Scores (empty)

---
+++ 2013-09-24-Test-Scores/js/main.js
@@ -0,0 +1,7 @@
+// Math.pow(number, power);
+//Math.sqrt(64);
+// see wikipedia for steps to calculate standard deviation.
+// input each of ten test scores
+// display the average and the standard deviation
+
+
