392f03c3bc0f3500009029bbb024c0898179a3e6
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-24 15:42:33 -0500

    Colors loop commit.

--- 2013-09-24-Colors-Loop/js/main.js
+++ 2013-09-24-Colors-Loop/js/main.js
@@ -1,8 +1,8 @@
 var colors = []
 
-debugger;
-
-var response = prompt('Enter a color or quit:');
+// debugger;
+var response = prompt('Enter a color:');
+var total_length = null;
 
 while(response)
 {
@@ -11,10 +11,20 @@ while(response)
 }
 
 
-/*
 
-do {
-  colors.push(prompt('Enter a color or quit:'));
-} while (response != 'quit');
+for(var count = 0; count < colors.length; count++)
+{
+  console.log("You typed in color: "+colors[count]);
+  //statements here;
+}
 
-*/
+debugger;
+
+for(var count = (colors.length - 1); count >= 0; count--)
+{
+  total_length += colors[count].length;
+}
+var avg = total_length / colors.length;
+
+console.log("total_length: "+total_length);
+console.log("avg: "+avg);
