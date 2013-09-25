d7ff6a371fa89616edd585e3f3cee1dbd13f5a22
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-25 18:01:32 -0500

    House MVP.

--- 2013-09-25-House/js/main.js
+++ 2013-09-25-House/js/main.js
@@ -1,7 +1,7 @@
 var rooms = [];
 var another = "";
-var window_cost = 250;
-var sq_ft_cost = 200;
+var window_price = 250;
+var sq_ft_price = 200;
 var house = {};
 
 // function total(propertyname, array)
@@ -25,6 +25,15 @@ function total_area()
   return sum;
 }
 
+function total_windows()
+{
+  var sum = 0;
+  for(var i = 0; i < rooms.length; i++)
+  {
+    sum += rooms[i].number_windows;
+  }
+  return sum;
+}
 
 do
 {
@@ -41,12 +50,18 @@ do
 /* Done:
 1. Get room objects from users.
 2. Room objects contain: Name, Length, Width, area, number_windows.
-3. Push to array called rooms
-4. Calculated: total_area,
+3. Pushed room objects (with properties) to array called rooms.
+4. Calculated and assigned the following to house object: number_rooms, total_area, total_windows.
 */
 
-house.total_area = total_area();
 house.number_rooms = rooms.length;
+house.total_area = total_area();
+house.total_windows = total_windows();
+house.windows_cost = house.total_windows * window_price;
+house.area_cost = house.total_area * sq_ft_price;
+house.total_cost = house.windows_cost + house.area_cost;
+
+
 
 
 
