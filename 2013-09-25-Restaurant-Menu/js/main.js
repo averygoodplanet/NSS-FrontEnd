415cb4ad5b66c7a25b85a6bbb85739c4dbb16573
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-26 16:05:37 -0500

    Restaurant: completed show_item() function.

--- 2013-09-25-Restaurant-Menu/js/main.js
+++ 2013-09-25-Restaurant-Menu/js/main.js
@@ -7,7 +7,7 @@ function user_creates_items ()
   {
   var item = {};
   item.Name = prompt("Name of the item: ");
-  item.Type = prompt("Type of item (e.g. side, veggies, chicken, chicken breast, beef, shrimp, appetizer, beverage): ");
+  item.Type = prompt("Type of item (e.g. side, veggies, chicken, beef, shrimp, appetizer, beverage): ");
   item.price = parseFloat(prompt("Price?: "));
   item.calories = parseFloat(prompt("Calories?: "));
   item.ingredients = prompt("Ingredients?: ");
@@ -16,5 +16,12 @@ function user_creates_items ()
   } while(another_item);
 }
 
+function show_item (item) //e.g. item --> "- Shrimp curry (shrimp peppers curry powder) $2.99"
+{
+  var item_statement = "- "+item.Name+" ("+item.ingredients+") $"+item.price;
+  return item_statement;
+}
+
 user_creates_items();
 console.log(menu);
+// console.log(show_item(menu[0]));
