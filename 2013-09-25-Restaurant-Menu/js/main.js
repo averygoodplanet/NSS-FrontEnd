6805c831179a40baf9596fcc60192889a03f301d
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-26 15:37:42 -0500

    Restaurant: do-while loop to add two or more items.

--- 2013-09-25-Restaurant-Menu/js/main.js
+++ 2013-09-25-Restaurant-Menu/js/main.js
@@ -1,14 +1,18 @@
 var menu = [];
+var another_item = "";
+// debugger;
 
-debugger;
-console.log("Hello");
 
-var item = {};
-item.Name = prompt("Name of the item: ");
-item.Type = prompt("Type of item (e.g. side, veggies, chicken, chicken breast, beef, shrimp, appetizer, beverage): ");
-item.price = parseFloat(prompt("Price?: "));
-item.calories = parseFloat(prompt("Calories?: "));
-item.ingredients = prompt("Ingredients?: ");
-menu.push(item);
+  do
+  {
+  var item = {};
+  item.Name = prompt("Name of the item: ");
+  item.Type = prompt("Type of item (e.g. side, veggies, chicken, chicken breast, beef, shrimp, appetizer, beverage): ");
+  item.price = parseFloat(prompt("Price?: "));
+  item.calories = parseFloat(prompt("Calories?: "));
+  item.ingredients = prompt("Ingredients?: ");
+  menu.push(item);
+  another_item = prompt("Enter anything to add another item, otherwise just press enter.");
+  } while(another_item);
 
-console.log("menu: "+menu);
+console.log(menu);
