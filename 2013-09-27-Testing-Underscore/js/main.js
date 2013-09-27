6c937bed69fb7299ab9bc3e23ad973961da886a4
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-27 14:25:11 -0500

    Underscore Testing: filter_odds passed.

--- 2013-09-27-Testing-Underscore/js/main.js
+++ 2013-09-27-Testing-Underscore/js/main.js
@@ -4,3 +4,8 @@ function filter_evens(numbers)
   //in _.filter, the anonymous function returns a boolean expression (i.e. returens true or false)
   // items returning true are add to the list of filtered-items to be retained.
 }
+
+function filter_odds(numbers)
+{
+  return _.filter(numbers, function(num){return (num % 2) != 0;});
+}
--- 2013-09-27-Testing-Underscore/js/testing.js
+++ 2013-09-27-Testing-Underscore/js/testing.js
@@ -3,3 +3,9 @@ test( "Filter Even Numbers", function () {
   var expected = _.range(0, 10, 2);  // 0, 2, 4, 6, 8;
   deepEqual(filter_evens(numbers), expected, "Message goes here i.e. testing the filter_evens function");
 });
+
+test( "Filter Odd Numbers", function () {
+  var numbers = _.range(10);
+  var expected = [1, 3, 5, 7, 9];
+  deepEqual(filter_odds(numbers), expected, "Testing the filter_odds function");
+});
