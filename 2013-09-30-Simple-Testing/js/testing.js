868daeefeebe880eaf56072f5379e522805c5b6c
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-30 09:45:05 -0500

    Simple-Testing: add_three passed.

--- 2013-09-30-Simple-Testing/js/testing.js
+++ 2013-09-30-Simple-Testing/js/testing.js
@@ -9,3 +9,11 @@ test( "1 + 1", function() {
   deepEqual( 1 + 1, 2, "adding 1 and 1");
 });
 
+test( "nashville[0]", function() {
+  deepEqual( "nashville"[0], "n" , "getting first letter from string");
+});
+
+test( "add_three", function() {
+  deepEqual( add_three(5), 8, "adding three to five");
+  deepEqual( add_three(7), 10, "adding three to five");
+});
