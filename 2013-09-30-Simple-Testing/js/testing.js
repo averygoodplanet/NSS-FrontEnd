838c1dca5ba64f026d7227304e7ce9f72530b127
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-30 09:34:12 -0500

    Starting 9-30 Test

---
+++ 2013-09-30-Simple-Testing/js/testing.js
@@ -0,0 +1,11 @@
+// test( "<name of function>", function() {
+//   deepEqual(<actual_value>, <expected_value>, "what this test is doing");
+//   deepEqual(<actual_value>, <expected_value>, "testing this function with different arguments");
+// });
+
+
+
+test( "1 + 1", function() {
+  deepEqual( 1 + 1, 2, "adding 1 and 1");
+});
+
