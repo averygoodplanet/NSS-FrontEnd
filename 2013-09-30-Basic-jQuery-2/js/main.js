a2684caf479aa72edc3d895ea6193c6de7de2f07
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-30 15:00:34 -0500

    Basic jQuery 2: measuring character count proper.

--- 2013-09-30-Basic-jQuery-2/js/main.js
+++ 2013-09-30-Basic-jQuery-2/js/main.js
@@ -4,6 +4,14 @@ function make_green(){
   $("#green").css("background-color", "green");
 }
 
+function count_characters(){
+  // debugger;
+  var name = $('#name_txt').val();
+  var name_length = name.length;
+  $("#name_div").text(name + "is " + name_length + " characters long.");
+}
+
 function initialize(){
   $("#button1").click(make_green);
+  $("#name_btn").click(count_characters);
 }
