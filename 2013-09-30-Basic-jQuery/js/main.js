65e57c0a8762088f3d985396d9ea3d7ab6c1616e
Peter Himmelreich <averygoodplanet@gmail.com>
2013-09-30 13:29:48 -0500

    Basic jQuery: user input changes text and background-color.

--- 2013-09-30-Basic-jQuery/js/main.js
+++ 2013-09-30-Basic-jQuery/js/main.js
@@ -10,9 +10,22 @@ function initialize() {
   // var size = prompt("What font-size (px)?");
   // $('div').css("font-size", size+"px");
 
-  var selector = prompt('Which div?');
-  var cls = prompt('Class to add?');
-  var new_text = prompt('What would you like to say?');
-  $(selector).text(new_text);
-  $(selector).addClass(cls);
+  // var selector = prompt('Which div?');
+  // var cls = prompt('Class to add?');
+  // var new_text = prompt('What would you like to say?');
+  // $(selector).text(new_text);
+  // $(selector).addClass(cls);
+
+  // var selector_to_hide = prompt("Which node do you want to hide?");
+  // $(selector_to_hide).hide();
+  function change_div_text(){
+    var name = $('#name').val();
+    var color = $('#color').val();
+    $('#b').text(name).css("background-color", color);
+    //chaining
+  }
+
+  $("#clicker").click(change_div_text); //**You are registering an event handler--do not put parens after alertme.
+  //.click(alert_me) -- run alert_me on click.
+  //.click(alert_me()) -- run alert immediately.
 }
