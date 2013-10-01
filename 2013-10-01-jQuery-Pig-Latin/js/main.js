8a7ca4923068bc193d4d5fb411060091c7ee15b9
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 10:36:56 -0500

    Pig Latin: working version; needs formatting.

--- 2013-10-01-jQuery-Pig-Latin/js/main.js
+++ 2013-10-01-jQuery-Pig-Latin/js/main.js
@@ -4,8 +4,12 @@ function convert_to_piglatin(word) {
   return word.slice(1) + word[0] + "a";
 }
 
-
+function displayresult() {
+  var original = $('#original').val();
+  var piglatin = convert_to_piglatin(original);
+  $('#piglatin').text(piglatin);
+}
 
 function initialize(){
-
+  $('#pig').click(displayresult);
 }
