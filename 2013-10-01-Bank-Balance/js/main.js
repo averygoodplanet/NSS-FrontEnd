ce547741df35f54f38ad357ea76fe14d0f9d5f2d
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 13:39:37 -0500

    Bank: working version, with .focus clear amount

--- 2013-10-01-Bank-Balance/js/main.js
+++ 2013-10-01-Bank-Balance/js/main.js
@@ -2,16 +2,26 @@ $(document).ready(initialize);
 
 var balance = 1000;
 
-function deposit(balance, deposit_amount){
+function deposit(){
+  var deposit_amount = parseFloat($('#amount').val());
   balance = balance + deposit_amount;
-  return balance;
+  $('#balance').val(balance);
 }
 
-function withdrawal(balance, withdrawal_amount){
+function withdrawal(){
+  var withdrawal_amount = parseFloat($('#amount').val());
   balance = balance - withdrawal_amount;
-  return balance;
+  $('#balance').val(balance);
+}
+
+function clear_amount(){
+  $('#amount').val('');
 }
 
 function initialize(){
-
+  $('#balance').val(balance);
+  $('#amount').val('');
+  $('#deposit').click(deposit);
+  $('#withdraw').click(withdrawal);
+  $('#amount').focus(clear_amount);
 }
