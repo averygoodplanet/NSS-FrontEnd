7498946554c7e1be8a137f0c6ed1d51ce2846a99
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 12:24:00 -0500

    Reverse PigLatin: reverse_word_order passed.

--- 2013-10-01-jQuery-Reverse-PigLatin/js/main.js
+++ 2013-10-01-jQuery-Reverse-PigLatin/js/main.js
@@ -4,6 +4,20 @@ function to_pig_latin(word){
   return word.slice(1) + word[0] + "a";
 }
 
+function comma_string_to_pl(string) {
+  var word_array = string.split(', ');
+  var new_array = [];
+  for(i in word_array)
+  {
+    new_array.push(to_pig_latin(word_array[i]));
+  }
+  return new_array.join(', ');
+}
+
+function reverse_word_order(string) {
+  //returns a comma-separated string of reverse word-order
+  return string.split(", ").reverse().join(', ')
+}
 
 function initialize () {
 
--- 2013-10-01-jQuery-Reverse-PigLatin/js/testing.js
+++ 2013-10-01-jQuery-Reverse-PigLatin/js/testing.js
@@ -1,3 +1,11 @@
 test("to_pig_latin", function() {
   deepEqual(to_pig_latin("hello"), "elloha", "hello to pig latin");
 });
+
+test("comma_string_to_pl", function() {
+  deepEqual(comma_string_to_pl("hello, nashville, code"), "elloha, ashvillena, odeca", "changing converting comma string to array pig latin");
+});
+
+test("reverse_word_order", function() {
+  deepEqual(reverse_word_order("elloha, ashvillena, odeca"), "odeca, ashvillena, elloha", "reverse_word_order");
+});
