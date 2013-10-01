403ed8f12609a69765345f7ac7e203c37488c331
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 11:43:05 -0500

    to_pig_latin passed.

---
+++ 2013-10-01-jQuery-Reverse-PigLatin/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(initialize);
+
+function to_pig_latin(word){
+  return word.slice(1) + word[0] + "a";
+}
+
+
+function initialize () {
+
+}
---
+++ 2013-10-01-jQuery-Reverse-PigLatin/js/testing.js
@@ -0,0 +1,3 @@
+test("to_pig_latin", function() {
+  deepEqual(to_pig_latin("hello"), "elloha", "hello to pig latin");
+});
