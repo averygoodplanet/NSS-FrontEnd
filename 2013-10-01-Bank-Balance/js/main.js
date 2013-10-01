616c92b65045f4e7b20bc5d2b4eca8556c51804a
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 13:53:57 -0500

    working version of website with .css(background-color red) for negative balance

--- 2013-10-01-Bank-Balance/js/main.js
+++ 2013-10-01-Bank-Balance/js/main.js
@@ -6,16 +6,25 @@ function deposit(){
   var deposit_amount = parseFloat($('#amount').val());
   balance = balance + deposit_amount;
   $('#balance').val(balance);
+  balance_below();
 }
 
 function withdrawal(){
   var withdrawal_amount = parseFloat($('#amount').val());
   balance = balance - withdrawal_amount;
   $('#balance').val(balance);
+  balance_below();
 }
 
 function clear_amount(){
   $('#amount').val('');
+}
+
+function balance_below() {
+  if(balance < 0)
+    $('#balance').css("background-color", "red");
+  else
+    $('#balance').css("background-color", "white");
 }
 
 function initialize(){
