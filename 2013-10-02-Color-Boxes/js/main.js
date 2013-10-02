2577bdf43b6a88a98518a4023f20e613b3a1f08d
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-02 11:59:35 -0500

    Color Boxes: added Clear Input button and on load .focus().

--- 2013-10-02-Color-Boxes/js/main.js
+++ 2013-10-02-Color-Boxes/js/main.js
@@ -8,10 +8,17 @@ function makeColorBoxes() {
     var $box = $('<div>');
     $box.addClass('box');
     $box.css('background-color', colors[i]);
+    $box.text(colors[i]);
+      if(colors[i] == 'white' || colors[i] == 'yellow')
+        $box.css('color', 'black');
     $('#boxes').append($box);
   }
 }
 
+
+function clearInput() {
+  $('#colorstring').val('');
+}
 
 
 
@@ -22,4 +29,9 @@ function makeColorBoxes() {
 
 function initialize() {
   $('#colorbutton').click(makeColorBoxes);
+  $('#colorstring').val('');
+  // a UX featuer (user-experience)
+  // on load, the cursor is already in the form
+  $('#colorstring').focus();
+  $('#clearinput').click(clearInput);
 }
