0716ff71095f6bcc0984817a13cf6b4187ebde01
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-02 11:01:24 -0500

    Making Boxes: added alternating ROYGBIV colors on boxes.

--- 2013-10-02-Making-Boxes/js/main.js
+++ 2013-10-02-Making-Boxes/js/main.js
@@ -3,10 +3,12 @@ $(document).ready(initialize);
 function makingBoxes() {
   var numberBoxes = parseInt($('#amount').val());
   var box_array = [];
+  var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
   for(var i = 0; i < numberBoxes; i++) {
-    var box = $('<div>');
-    box.addClass('box');
-    $("#boxes").append(box);
+    var $box = $('<div>');
+    $box.addClass('box');
+    $box.css('background-color', colors[i%7]);
+    $("#boxes").append($box);
   }
 }
 
