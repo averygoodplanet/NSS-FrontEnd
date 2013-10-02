4ab71472ce9297ac1977b0184eac090c93b0c667
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 21:33:30 -0500

    make_multiples_array works

--- 2013-10-01-Sum-Multiples/js/main.js
+++ 2013-10-01-Sum-Multiples/js/main.js
@@ -1,10 +1,9 @@
 $(document).ready(initialize);
 
-var a = 0;
-var b = 0;
 
 function userinput_to_a_b () {
   var user_string = $('#userinput').val();
+  // var user_string = "5,3";  //for testing
   var array_strings = user_string.split(',');
   var array_numbers = [];
   for(i in array_strings)
@@ -17,8 +16,17 @@ function a_to_range (a) { // e.g. if a = 5,  range = [1,2,3,4,5]
   return _.range(1, a+1);
 }
 
-
+function make_multiples_array() { // e.g. [3,6,9,12,15]
+  var array_a_b = userinput_to_a_b();
+  var a = array_a_b[0];
+  var b = array_a_b[1];
+  var range = a_to_range(a);
+  var multiples_array = [];
+  for(i in range)
+    multiples_array.push((range[i])*b);
+  return multiples_array;
+}
 
 function initialize(){
-
+  make_multiples_array();
 }
--- 2013-10-01-Sum-Multiples/js/testing.js
+++ 2013-10-01-Sum-Multiples/js/testing.js
@@ -5,3 +5,7 @@
 test("a_to_range",  function () {
   deepEqual(a_to_range(5), [1,2,3,4,5], "converting 5 to [1,2,3,4,5]");
 });
+
+test("make_multiples_array", function() {
+  deepEqual(make_multiples_array(), [3,6,9,12,15], "converting 5,3 to [3,6,9,12,15]");
+});
