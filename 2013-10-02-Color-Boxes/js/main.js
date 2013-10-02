a756365cabc1a074dbac9c38133ae427186a6058
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-02 11:38:14 -0500

    Color Boxes: minimum viable product.

---
+++ 2013-10-02-Color-Boxes/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(initialize);
+
+function makeColorBoxes() {
+  var colorstring = $('#colorstring').val();
+  console.log(colorstring);
+  var colors = colorstring.split(', ');
+  for(i in colors){
+    var box = $('<div>');
+    box.addClass('box');
+    box.css('background-color', colors[i]);
+    $('#boxes').append(box);
+  }
+}
+
+
+
+
+
+
+
+
+
+function initialize() {
+  $('#colorbutton').click(makeColorBoxes);
+}
