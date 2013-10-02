009873d3031ca5d96256713a914ee66511e199bb
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-02 15:53:09 -0500

    Photoshop Lite: staring html structure for new features.

--- 2013-10-02-Photoshop-Lite/js/main.js
+++ 2013-10-02-Photoshop-Lite/js/main.js
@@ -1,27 +1,27 @@
 $(document).ready(initialize);
 
+function clearInputAndFocus () {
+  $('#color').val('');
+  $('#color').focus('');
+}
+
 function addColor() {
   var $box = $('<div>');
   var box_color = $('#color').val();
   $box.addClass('box');
   $box.css('background-color', box_color);
   $('#colors').prepend($box);
-  $('#color').val('');
-  $('#color').focus();
+  clearInputAndFocus();
 }
 
-
-
-
 function initialize() {
-  $('#color').val('');
-  $('#color').focus();
+  clearInputAndFocus();
   $('#addColor').click(addColor);
-
 
   //on Enter keypress call addColor function.
   $('#color').keypress(function (event) {
     if(event.which == 13)
       addColor();
   });
+
 }
