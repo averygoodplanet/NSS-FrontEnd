81047e64d3d90dd366611453cc9ab7a5837bbe95
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-01 21:57:57 -0500

    Initial working version

--- 2013-10-01-Sum-Multiples/js/main.js
+++ 2013-10-01-Sum-Multiples/js/main.js
@@ -7,7 +7,6 @@ function userinput_to_a_b () {
   var array_numbers = [];
   for(i in array_strings)
     array_numbers.push(parseInt(array_strings[i]));
-  console.log("array_numbers: "+array_numbers);
   return array_numbers; // e.g. [5, 3]
 }
 
@@ -15,8 +14,6 @@ function a_to_range (a) { // e.g. if a = 5,  range = [1,2,3,4,5]
   return _.range(1, a+1);
 }
 
-//make_multiples_array uses functions:
-// 1) userinput_to_a_b, 2) a_to_range.
 function make_multiples_array() { // e.g. [3,6,9,12,15]
   var array_a_b = userinput_to_a_b();
   var a = array_a_b[0];
@@ -35,14 +32,15 @@ function sum_array(array) {
   return sum;
 }
 
+// format_output_string function, combines all the functions above.
 function format_output_string() {
   return make_multiples_array().join('+') + "=" + sum_array(make_multiples_array()).toString();
 }
 
 function overall_output_function() {
-
+  $('#output').val(format_output_string());
 }
 
 function initialize() {
-  alert(format_output_string());
+  $('#calculate').click(overall_output_function);
 }
