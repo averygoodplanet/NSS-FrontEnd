4354ae27173052c70ee7d2dd4a94259d7cb11483
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-07 14:41:03 -0500

    Slideshow: initial setup.

---
+++ 2013-10-07-Slideshow/js/app/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+$(document).ready(initialize);
+
+function initialize(){
+  $(document).foundation();
+  $('#search').click(searchFlickr);
+}
+
+function searchFlickr() {
+  var API_KEY = 'f4c1acbf5ca4055d41391a532ca1954d';
+  var PER_PAGE = 500;
+  var page = 1;
+
+  var query = $('#query').val();
+  var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&text=' + query + '&per_page=' + PER_PAGE + '&page=' + page + '&format=json&jsoncallback=?';
+  // get JSON is asynchronous; results is the callback function; the data is passed to the callback function;
+  $.getJSON(url, results);
+}
+
+function results(data) {
+  for(var i = 0; i < data.photos.photo.length; i++){
+    createImage(data.photos.photo[i]);
+  }
+}
+
+function createImage(photo) {
+  var url = 'url(http://farm' + photo.farm + '.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
+  var $div = $('<div>');
+  $div.addClass('photo');
+  $div.css('background-image', url);
+  $div.css('background-size', 'cover');
+  $('#photos').prepend($div);
+}
