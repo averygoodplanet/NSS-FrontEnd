a29368701744a258e8801b8126a063ec80fd1bd1
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-07 15:16:42 -0500

    Slideshow: multipage timer working.

--- 2013-10-07-Slideshow/js/app/app.js
+++ 2013-10-07-Slideshow/js/app/app.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var photos = [];
+var currentIndex = 0;
+var timer = 0;
+var PER_PAGE = 5;
+var page = 1;
+var total_pages = 2;
+
 $(document).ready(initialize);
 
 function initialize(){
@@ -9,9 +16,6 @@ function initialize(){
 
 function searchFlickr() {
   var API_KEY = 'f4c1acbf5ca4055d41391a532ca1954d';
-  var PER_PAGE = 500;
-  var page = 1;
-
   var query = $('#query').val();
   var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&text=' + query + '&per_page=' + PER_PAGE + '&page=' + page + '&format=json&jsoncallback=?';
   // get JSON is asynchronous; results is the callback function; the data is passed to the callback function;
@@ -19,16 +23,32 @@ function searchFlickr() {
 }
 
 function results(data) {
-  for(var i = 0; i < data.photos.photo.length; i++){
-    createImage(data.photos.photo[i]);
-  }
+  photos = data.photos.photo;
+  timer = setInterval(createImage, 1000);
 }
 
-function createImage(photo) {
-  var url = 'url(http://farm' + photo.farm + '.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
-  var $div = $('<div>');
-  $div.addClass('photo');
-  $div.css('background-image', url);
-  $div.css('background-size', 'cover');
-  $('#photos').prepend($div);
+function resetGlobals() {
+  page++;
+  timer = 0;
+  photos = [];
+  currentIndex = 0;
+}
+
+function createImage() {
+  if(currentIndex < photos.length){
+    var photo = photos[currentIndex];
+    currentIndex++;
+    var url = 'url(http://farm' + photo.farm + '.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
+    var $div = $('<div>');
+    $div.addClass('photo');
+    $div.css('background-image', url);
+    $div.css('background-size', 'cover');
+    $('#photos').prepend($div);
+  } else {
+    window.clearInterval(timer);
+    if(page < total_pages){
+      resetGlobals();
+      searchFlickr();
+    }
+  }
 }
