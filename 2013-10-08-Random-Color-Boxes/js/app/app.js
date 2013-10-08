0ab09e98d585a9703a47050bf25e76119eae6b39
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-08 10:48:09 -0500

    makes white boxes of proper length and height

--- 2013-10-08-Random-Color-Boxes/js/app/app.js
+++ 2013-10-08-Random-Color-Boxes/js/app/app.js
@@ -16,4 +16,13 @@ function start() {
 
 function makeBoxes() {
   console.log('timer: '+timer);
+  var dimensionsString = $('#dimensions').val();
+  var dimensions = dimensionsString.split(', ');
+  var length = parseFloat(dimensions[0]);
+  var width = parseFloat(dimensions[1]);
+  var $box = $('<div>');
+  $box.addClass('box');
+  $box.css('height', length);
+  $box.css('width', width);
+  $('#colors').prepend($box);
 }
