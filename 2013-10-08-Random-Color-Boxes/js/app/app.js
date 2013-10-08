0a5cdaae92395726ea1beef3f4bef556d8ab8d98
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-08 10:28:49 -0500

    clicking start starts timer.

--- 2013-10-08-Random-Color-Boxes/js/app/app.js
+++ 2013-10-08-Random-Color-Boxes/js/app/app.js
@@ -1,7 +1,19 @@
 'use strict';
+var timer = 0;
+
 
 $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
+  $('#start').click(start);
+}
+
+function start() {
+  var delay =  parseInt($('#delay').val(), 10) * 1000;
+  timer = setInterval(makeBoxes, delay);
+}
+
+function makeBoxes() {
+  console.log('timer: '+timer);
 }
