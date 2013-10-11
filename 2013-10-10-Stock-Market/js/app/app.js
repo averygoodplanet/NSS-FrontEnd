62da99204757864536cf7039afe02c3553a51793
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-10 22:36:46 -0500

    Stocks: in buy() created stock object e.g. db.stocks.APPL = {...}

--- 2013-10-10-Stock-Market/js/app/app.js
+++ 2013-10-10-Stock-Market/js/app/app.js
@@ -3,23 +3,28 @@
 //Firebase Schema
 var Δdb;
 var Δbalance;
+var Δstocks;
 
 //Local Schema
 var db = {};
 db.balance = 0;
+db.stocks = {};
 
 $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
   Δdb = new Firebase('https://stocks-ph.firebaseio.com/');
-  Δbalance = Δdb.child('balance');
 
+  Δbalance = Δdb.child('balance');
   //once get the balance from database.
   Δbalance.once('value', loadBalance);
   $('#setBalance').click(setAndDisplayBalance);
 
-  // getStockQuote();
+  //creates a database Stocks OBJECT-not an array
+  Δstocks = Δdb.child('stocks');
+  // when they click "Buy button", add a stock
+  $('#buy').click(buy);
 }
 
 function loadBalance(snapshot) {
@@ -28,11 +33,39 @@ function loadBalance(snapshot) {
 }
 
 function setAndDisplayBalance() {
+  //getting balance from user's input
   var balance = parseFloat($('#inputBalance').val());
+  // storing balance in global variable (in propery balance of global db)
   db.balance = balance;
+  // setting balance in remote database
   Δbalance.set(balance);
+  //displaying balance
   $('#displayedBalance').val(balance);
+  // clearing the input field
   $('#inputBalance').val('');
+}
+
+function buy() {
+  //--get symbol and #-of-shares from user input
+  var symbol = $('#enterSymbol').val();
+  var count = $('#enterCount').val();
+
+  /*
+  --Store symbol and #-of-shares in global stocks object and in stocks database object.
+  The individual stock will be an object that is a property of these objects.
+  The property name will be the stock symbol. (In case at some point I want to
+  retrieve an individual stock by stock symbol.)
+  *Using bracket notation to set the propertyname to a string variable.
+  e.g. db.stocks.APPL = {symbol: APPL, count: 2};
+  */
+  db.stocks[symbol] = {symbol: symbol, count: count};
+
+  // fire JSON request to getStockQuote--pass in addNewStock as callback function.
+
+
+  // var stock = {symbol: 'AAPL', name: 'Apple', date: '1/1/2012', count: 1, subtotal: 100};
+  // Δstocks.set(stock);
+  // Δstocks.child(stock.symbol).set(stock);
 }
 
 function getStockQuote() {
