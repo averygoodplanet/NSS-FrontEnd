d5fe1000757473cbced192a328487465455c8cf9
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-10 23:38:56 -0500

    Stocks: in addNewStock loaded stocks to global variable and to database.

--- 2013-10-10-Stock-Market/js/app/app.js
+++ 2013-10-10-Stock-Market/js/app/app.js
@@ -61,20 +61,15 @@ function storeSymbolAndCount() {
   var symbol = $('#enterSymbol').val();
   var count = $('#enterCount').val();
 
-  /*
-  --Store symbol and #-of-shares in global stocks object and in stocks database object.
-  The individual stock will be an object that is a property of these objects.
-  The property name will be the stock symbol. (In case at some point I want to
-  retrieve an individual stock by stock symbol.)
-  *Using bracket notation to set the propertyname to a string variable.
-  e.g. db.stocks.APPL = {symbol: APPL, count: 2};
-  */
+  // We do not store stock to database until we receive API results.
+  // We store the count because we will need it in addNewStock().
   db.stocks[symbol] = {symbol: symbol, count: count};
-  Δstocks.child(symbol).set({symbol: symbol, count: count});
+
   return symbol;
 }
 
 function addNewStock(data) {
+  // extract info from API data and repackage into a local stock variable.
   var stockSymbol = data.Data.Symbol;
   var stockCount = db.stocks[stockSymbol].count;
   var subtotal = data.Data.LastPrice * stockCount;
@@ -85,9 +80,15 @@ function addNewStock(data) {
                 count: stockCount,
                 subtotal: subtotal,
                 changePositive: change };
-  debugger;
-  // var symbol = data.symbol;
-  // console.log(symbol);
+
+  /* Store the new-stock into a global variable and in remote database.
+    *Using bracket notation to set the propertyname to a string variable, which will be the stock symbol,
+    in case I later want to retrieve a stock's info by it's stock symbol.
+    e.g. db.stocks.APPL = {symbol: APPL, count: 2};
+   NOTE: we will use database child_added or updated to trigger displaying stocks in the page table.
+  */
+  db.stocks[stockSymbol] = stock;
+  Δstocks.child(stockSymbol).set(stock);
 }
 
 function getStockQuote(symbol, fn) {
