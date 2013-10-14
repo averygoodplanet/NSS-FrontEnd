f35b15070dd2d0372f5b4aeda791518dae54e7d3
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-14 14:54:30 -0500

    Add Location starts.

--- 2013-10-14-Vacation-Map/public/js/app/app.js
+++ 2013-10-14-Vacation-Map/public/js/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Firebase Schema
-var Δdb;
+var Δdb, Δlocations;
 
 // Local Schema
 var db = {};
@@ -13,16 +13,68 @@ $(document).ready(initialize);
 function initialize(){
   $(document).foundation();
   Δdb = new Firebase('https://vacation-app.firebaseio.com');
-  var Δlocations = Δdb.child('locations');
+  Δlocations = Δdb.child('locations');
   Δlocations.on('child_added', dbLocationAdded);
   initMap(36, -86, 5);
+  $('#set-zoom').click(clickSetZoom);
+  $('#add-location').click(clickAddLocation);
+}
+
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+
+function initMap(lat, lng, zoom){
+  var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
+  db.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 function dbLocationAdded() {
 
 }
 
-function initMap(lat, lng, zoom){
-  var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
-  db.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+function clickSetZoom() {
+  var zoom = getValue('#zoom', parseInt);
+  db.map.setZoom(zoom);
 }
+
+function clickAddLocation() {
+  var name = getValue('#location');
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({address: name}, function(results, status) {
+    var location = {};
+    location.name = results[0].formatted_address;
+    location.coordinates = results[0].geometry.location;
+    Δlocations.push(location);
+  });
+}
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+function getValue(selector, fn) {
+  var value = $(selector).val();
+  $(selector).val('');
+
+  if(fn){
+    value = fn(value);
+  }
+
+  return value;
+}
+
+function parseUpperCase(string){
+  return string.toUpperCase();
+}
+
+function parseLowerCase(string){
+  return string.toLowerCase();
+}
+
+
