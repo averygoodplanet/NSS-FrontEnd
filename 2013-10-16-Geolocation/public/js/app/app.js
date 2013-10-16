e731ead57d58170100027be74719d0fb08aa915a
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-16 11:01:37 -0500

    Geolocation: getting location and pushing to firebase.

--- 2013-10-16-Geolocation/public/js/app/app.js
+++ 2013-10-16-Geolocation/public/js/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Firebase Schema
-var Δdb;
+var Δdb, Δpositions;
 
 // Local Schema (defined in keys.js)
 
@@ -10,7 +10,28 @@ $(document).ready(initialize);
 function initialize(){
   $(document).foundation();
   Δdb = new Firebase(db.keys.firebase);
+  Δpositions = Δdb.child('positions');
+  Δpositions.on('child_added', dbPositionAdded);
   initMap(36, -86, 5);
+  $('#start').click(clickStart);
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+function dbPositionAdded(snapshot) {
+  console.log('snapshot in dbPositionAdded: ');
+  console.log(snapshot.val());
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+function clickStart(){
+  //determine our location
+  var geoOptions = { enableHighAccuracy: true, maximumAge: 1000, timeout: 60000};
+
+  db.watchId = navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
 }
 
 // -------------------------------------------------------------------- //
@@ -21,6 +42,21 @@ function initMap(lat, lng, zoom){
   var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
   db.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
+
+function geoSuccess(location) {
+  var position = {};
+  position.latitude = location.coords.latitude;
+  position.longitude = location.coords.longitude;
+  position.altitude = location.coords.altitude || 0;
+  position.time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  Δpositions.push(position);
+}
+
+function geoError() {
+  console.log('Sorry, no position available.');
+}
+
+
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
