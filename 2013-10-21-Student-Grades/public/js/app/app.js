f78fac94d1c534fac5842a3be4c6eeb2076cde8d
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-21 14:38:34 -0500

    student grades end of class Monday

---
+++ 2013-10-21-Student-Grades/public/js/app/app.js
@@ -0,0 +1,111 @@
+'use strict';
+
+//Local Schema (defined in keys.js)
+var schools = [];
+
+$(document).ready(initialize);
+
+function initialize(fn, flag){
+  if(!canRun(flag)) {return;}
+
+  $(document).foundation();
+  $('#add-school').click(clickAddSchool);
+  $('#add-student').click(clickAddStudent);
+  $('#add-subject').click(clickAddSubject);
+}
+
+function clickAddSubject() {
+  var subjectName = getValue('#subject');
+  var studentName = $('#pick-student').val();
+
+  debugger;
+}
+
+function clickAddSchool() {
+  var name = getValue('#school');
+  var length = getValue('#length');
+  var width = getValue('#width');
+  var school = new School(name, length, width);
+  schools.push(school);
+  htmlAddSchoolToSelect(school);
+}
+
+function clickAddStudent() {
+  var name = getValue('#student');
+  var schoolName = $('#pick-school').val();
+  var gpa = getValue('#gpa', parseFloat);
+
+  //return the school object for name e.g. 'MIT'
+  var school = _.find(schools, function(s){
+    return s.name === schoolName;
+  });
+
+  //create a student and add to school.students
+  var student = new Student(name, gpa);
+  school.students.push(student);
+  htmlAddStudentToSelect(student);
+}
+// -----------------------------------------------------------------//
+// CLASSES DEFINED HERE //
+
+function School(name, length, width) {
+  // "this" refers to itself, i.e. School.name;
+  this.name = name;
+  this.established = '1930';
+  this.length = length;
+  this.width = width;
+  this.students = [];
+  //making the area value "live" throught creating a function.
+  //to get area call this.area()  the parentheses.
+  this.area = function() {return parseInt(this.length, 10) * parseInt(this.width, 10);};
+  this.gpa = function(){
+    var sum = _.reduce(this.students, function(memo, student) {return memo + student.gpa;}, 0);
+    var total = this.students.length;
+    return sum /total;
+  };
+}
+
+function Student(name, gpa){
+  this.name = name;
+  this.gpa = gpa;
+  this.subjects = [];
+}
+// -----------------------------------------------------------------//
+
+function htmlAddSchoolToSelect(school){
+  var $option = $('<option>');
+  $option.text(school.name);
+  $option.val(school.name);
+  $('#pick-school').append($option);
+}
+
+function htmlAddStudentToSelect(school){
+  var $option = $('<option>');
+  $option.text(student.name);
+  $option.val(student.name);
+  $('#pick-student').append($option);
+}
+
+
+
+//-----------------------------------------------------------------//
+
+function getValue(selector, fn){
+  var value = $(selector).val();
+  value = value.trim();
+  $(selector).val('');
+
+  if(fn){
+    value = fn(value);
+  }
+
+  return value;
+}
+
+function canRun(flag){
+  var isQunit = $('#qunit').length > 0;
+  var isFlag = flag !== undefined;
+  var value = isQunit && isFlag || !isQunit;
+  return value;
+}
+
