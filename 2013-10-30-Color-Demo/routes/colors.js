adb80bbcbaa13d1db5ff6490c01f59fb908959c4
Peter Himmelreich <averygoodplanet@gmail.com>
2013-10-30 10:17:52 -0500

    equal widths colors

--- 2013-10-30-Color-Demo/routes/colors.js
+++ 2013-10-30-Color-Demo/routes/colors.js
@@ -1,5 +1,6 @@
 // GET /colors
 
 exports.index = function(req, res){
-  res.render('colors/index', {title: "colors"});
+  var colors = ['blue', 'green', 'orange', 'olive', 'red'];
+  res.render('colors/index', {title: "colors", colors: colors});
 };
